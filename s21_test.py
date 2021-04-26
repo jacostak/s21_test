1e9f5ea7feae758f3247c10b9f2f1152735854f2
jacostak <jacostak@uoregon.edu>
2021-04-26 10:57:16 -0700

    Update s21_test.py

--- s21_test.py
+++ s21_test.py
@@ -1,1 +1,7 @@
+def remove_list_item(*, the_list, the_item):
+  ''' create new list containing all items, except the_item 
+  to be deleted.'''
 
+  new_list = [item for item in the_list if item != the_item]
+  return new_list
+  
